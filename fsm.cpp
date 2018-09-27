3c330cb12c391502b769a6f6fb6f271bed3ee685
Michael Li <limichael1099419@gmail.com>
2018-09-27 14:13:37 -0700

    added some error checking and std

--- fsm.cpp
+++ fsm.cpp
@@ -4,105 +4,127 @@ int FSM_ID(char* buffer)	//CALL WHEN THE FIRST CHAR IS A LETTER
 {
 	int flag = 0;
 
+	if (isalpha(buffer[0])!= 0)		//error checking if the first letter is a letter
+	{
+		std::cerr << "First character is not a letter\n";
+		return EXIT_FAILURE; 
+	}
+
+
 	
-	if(isKeyword(buffer))		//if it is an identifier
+	
+
+
+	if (isKeyword(buffer))		//if it is an identifier
 	{
 		flag = 2;		//keyword
 	}
-	else 
+	else
 	{
 		flag = 1; 		//an identifier, but not a keyword
-	}	
+	}
 
-	return flag; 			
+	return flag;
 }
 
 int FSM_INT(char* buffer)	//CALL WHEN THE FIRST CHAR IS A DIGIT
 {
-	int flag = 0; 	
+	int flag = 0;
+
+	if (isdigit(buffer[0]) != 0)	
+	{
+		std::cerr << "First character is not a digit\n";
+		return EXIT_FAILURE;
+	}
+
+
+
 
 	
+	if (FSM_REAL(buffer))  //real vs integers //call when encountering a decimal point 
+	{
+			flag = 2;
+	}
 
-	return flag; 
+
+	return flag;
 }
 
-int FSM_REAL(char* buffer)	//CALL IN INT- CHECK FOR DECIMALS 
+int FSM_REAL(char* buffer)	//CALL IN FSM_INT- CHECK FOR DECIMALS 
 {
-	int flag = 0; 
+	int flag = 0;
 
 
 
-	return flag; 
+	return flag;			//returns 1 if it is a real number 
 }
 
 void Unknown()		//CALL WHEN NOT A TOKEN
 {
-	
+
 
 }
 
-int isKeyword   ( char* buffer)	//CALL IN FSM_ID
+int isKeyword(char* buffer)	//CALL IN FSM_ID
 
 {	//Feel free to add more if needed
-	vector<string> keywords  = {
-		"if", "ifend", "put", "get", "else", 
-		"while", "whileend", "return", "true", "false" 
-	};	
-	
-	int flag = 0;
-
-	for(int i = 0; i< keywords.length();i++)
-	{
-		if(strcmp(buffer, keywords[i].c_str()) == 0)
-		{
-			flag = 1; 
-			break;
-		}
-	}
-	
-	return flag; 
-}
-
-
-int isOperator (char * buffer)
-{	//feel free to add if needed
-	vector<string> operators = {
-		"+", "-", "*", "=", "==", "^=",
-		">", "<", "=>", "=<"
-	};	
-	
-	int flag = 0;
-
-	for(int i = 0; i< operators.length();i++)
-	{
-		if(strcmp(buffer, operators[i].c_str()) == 0)
-		{
-			flag = 1; 
-			break;
-		}
-	}
-	
-	return flag; 
-
-}
-
-int isSeparator (char * buffer)
-{	//feel free to add more
-	vector<string> separators  = {
-		"$$", " ", ":", ",", ";",
-		"{}", "::=", "|", "/"			 
+	std::vector<std::string> keywords = {
+		"if", "ifend", "put", "get", "else",
+		"while", "whileend", "return", "true", "false"
 	};
 
 	int flag = 0;
 
-	for(int i = 0; i< separators.length();i++)
+	for (int i = 0; i< keywords.size(); i++)
 	{
-		if(strcmp(buffer, separators[i].c_str()) == 0)
+		if (strcmp(buffer, keywords[i].c_str()) == 0)
 		{
-			flag = 1; 
+			flag = 1;
+		}
+	}
+
+	return flag;
+}
+
+
+int isOperator(char * buffer)
+{	//feel free to add if needed
+	std::vector<std::string> operators = {
+		"+", "-", "*", "=", "==", "^=",
+		">", "<", "=>", "=<"
+	};
+
+	int flag = 0;
+
+	for (int i = 0; i< operators.size(); i++)
+	{
+		if (strcmp(buffer, operators[i].c_str()) == 0)
+		{
+			flag = 1;
+		}
+	}
+
+	return flag;
+
+}
+
+int isSeparator(char * buffer)
+{	//feel free to add more
+	std::vector<std::string> separators = {
+		"$$", " ", ":", ",", ";",
+		"{}", "::=", "|", "/"
+	};
+
+	int flag = 0;
+
+	for (int i = 0; i< separators.size(); i++)
+	{
+		if (strcmp(buffer, separators[i].c_str()) == 0)
+		{
+			flag = 1;
 			break;
 		}
 	}
-	
-	return flag; 
+
+	return flag;
 }
